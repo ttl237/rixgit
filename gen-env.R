a30d3d92beec4f3d5404f939f579b112d041a96b
ttl237 <lagloire.tsokezo.001@student.uni.lu>
2026-01-27 15:41:55 +0100

    Add reproducible environment spec (rix + Nix)

--- gen-env.R
+++ gen-env.R
@@ -5,15 +5,48 @@
 library(rix)
 
 # Define execution environment
+
 rix(
-  date = NULL,
-  r_pkgs = NULL,
-  py_conf = NULL,
+  date = "2026-01-19",
+  r_pkgs = c(
+    "dplyr",
+    "ggplot2",
+    "reticulate",
+    "yardstick",
+    "testthat",
+    "withr"
+  ),
   git_pkgs = list(
-    "package_name" = "rixpress",
-    "repo_url" = "https://github.com/ropensci/rixpress",
-    "commit" = "4d5da094c2dc33d13f9b4966b9c502b328086714",
+    list(
+      package_name = "rix",
+      repo_url = "https://github.com/ropensci/rix/",
+      commit = "HEAD"
+    ),
+    list(
+      package_name = "rixpress",
+      repo_url = "https://github.com/ropensci/rixpress",
+      commit = "HEAD"
+    )
+  ),
+  py_conf = list(
+    py_version = "3.13",
+    py_pkgs = c(
+      "numpy",
+      "pandas",
+      "scikit-learn",
+      "matplotlib",
+      "seaborn",
+      "pytest"
+    ),
+    git_pkgs = list(
+      list(
+        package_name = "ryxpress",
+        repo_url = "https://github.com/b-rodrigues/ryxpress",
+        commit = "HEAD"
+      )
+    )
   ),
   ide = "none",
-  project_path = "."
+  project_path = ".",
+  overwrite = TRUE
 )
